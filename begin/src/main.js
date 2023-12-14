119b2b1dfa1be68f586b4a9c6ddc1cc75091467b
ly <1563694059@qq.com>
2023-12-14 13:39:48 +0800

    线上样式错乱修复

--- begin/src/main.js
+++ begin/src/main.js
@@ -2,20 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from "./router/index";
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-import  'amfe-flexible'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import  './ui-FZ/animation.css'
 import axios from "axios";
+import  'amfe-flexible'
 import {message} from './ui-FZ/message'
 import  request from './axios/request'
-import  './ui-FZ/animation.css'
 import  store from './util/index.js'
-Vue.prototype.$request = request
-Vue.use(Antd)
+import router from "./router/index";
 Vue.use(ElementUI);
+Vue.prototype.$request = request
 Vue.config.productionTip = false
 Vue.prototype.$message = message
 /* eslint-disable no-new */
