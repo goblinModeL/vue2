fb63312f1264a665a3e7ec343b3e255cf993d822
ly <1563694059@qq.com>
2024-04-09 17:11:30 +0800

    WebSocket

--- begin/ob.js
+++ begin/ob.js
@@ -36,19 +36,21 @@
 // console.log(newdata3)
 //
 //
-const  url="http://192.168.99.6/list/name?num=1234&age=999"
-let value=url.split('?')[1].split('&')
-console.log(value)
-const  end=[
-]
-value.forEach(item=>{
-  console.log(item)
- let uk= item.split("=")
-  let newdata={}
-  newdata[uk[0]]=uk[1]
-  end.push(newdata)
-})
-console.log(end)
+
+
+// const  url="http://192.168.99.6/list/name?num=1234&age=999"
+// let value=url.split('?')[1].split('&')
+// console.log(value)
+// const  end=[
+// ]
+// value.forEach(item=>{
+//   console.log(item)
+//  let uk= item.split("=")
+//   let newdata={}
+//   newdata[uk[0]]=uk[1]
+//   end.push(newdata)
+// })
+// console.log(end)
 //
 //
 //
@@ -90,20 +92,153 @@ console.log(end)
 // let b=[...a];
 // b.push(6)
 // console.log(a)
-let a=[1,2,3,4]
-let b=a.splice(1,1)
-console.log(a)
-console.log(b)
-let aa=[1,2,3,4]
-let bb=aa.slice(0,1)
-console.log(aa)
-console.log(bb)
-let s='www.baidu.com?a=3&b=10'
-let k=s.split('?')[1].split('&')
-console.log(k)
-let u=k.reduce((arry,item)=> ([key,value]=item.split('='),arry[key] = value, arry),{})
-console.log(u)
-字体渐变
+// let a=[1,2,3,4]
+// let b=a.splice(1,1)
+// console.log(a)
+// console.log(b)
+// let aa=[1,2,3,4]
+// let bb=aa.slice(0,1)
+// console.log(aa)
+// console.log(bb)
+// let s='www.baidu.com?a=3&b=10'
+// let k=s.split('?')[1].split('&')
+// console.log(k)
+// let u=k.reduce((arry,item)=> ([key,value]=item.split('='),arry[key] = value, arry),{})
+// console.log(u)
+// 字体渐变
 // background: linear-gradient(90deg, #FFFFFF 0%, #95D4FA 100%);
 // -webkit-background-clip: text;
 // -webkit-text-fill-color: transparent;
+// 创建一个 Promise 对象
+
+
+
+// const myPromise = new Promise((resolve, reject) => {
+//   // 异步操作
+//   setTimeout(() => {
+//     const randomNum = Math.random();
+//     console.log(randomNum)
+//     if (randomNum < 0.5) {
+//       // 如果随机数小于0.5，则将 Promise 标记为成功，并返回结果
+//       resolve("Success!");
+//     } else {
+//       // 如果随机数大于等于0.5，则将 Promise 标记为失败，并返回错误信息
+//       reject("Error!");
+//     }
+//   }, 1000); // 模拟异步操作，1秒后执行
+// });
+//
+// // 使用 Promise 对象
+// myPromise.then(
+//   // 成功时的处理函数
+//   result => {
+//     console.log("Promise resolved with result:", result);
+//   },
+//   // 失败时的处理函数
+//   error => {
+//     console.error("Promise rejected with error:", error);
+//   }
+// );
+
+// function fn1() {
+//   var a = 2;
+//   var b = 3;
+//   function fn2() {
+//     a++;
+//     console.log(a);
+//
+//   }
+//   return fn2;
+// }
+// var f = fn1;
+// console.log(f())
+// f();
+// f();
+// f = null;
+// var test = (function() {
+//   var num = 0
+//   return () => {
+//     return num++
+//   }
+// }())
+// for (var i = 0; i < 20; i++) {
+//   test()
+// }
+// console.log(test());
+// var age=100
+// var obj = {
+//   name:"lynn",
+//   age:18,
+//   showLen: function() {
+//     console.log(this.age);
+//   },
+//   fn:function(){
+//     (function(cb) {
+//       cb();
+//     })(this.showLen)
+//   }
+// };
+//
+// obj.fn();
+// function ReverseList(head) {
+//   let list = null; // list 用于保存反转后的链表
+//   let cur = head; // cur 用于遍历原链表的节点
+//
+//   while (cur) {
+//     let temp = cur.next; // 临时保存 cur.next，因为在修改 cur.next 后会丢失原始值
+//     // 将 cur 的 next 指针指向反转链表头部 list
+//     cur.next = list;
+//    console.log(list)
+//     // 更新 list 为当前节点，相当于将当前节点添加到反转链表的头部
+//     list = cur;
+//     console.log(list)
+//     // 移动 cur 到下一个节点
+//     cur = temp;
+//     console.log(cur)
+//     console.log('\n')
+//   }
+//
+//   return list; // 返回反转后的链表头部
+// }
+// let Head={
+//   val:1,
+//   next:{
+//     val:2,
+//     next:{
+//       val:3,
+//       next:{
+//         val:4,
+//         next:{
+//           val:5,
+//           next:null
+//         }
+//       }
+//     }
+//   }
+// }
+// ReverseList(Head)
+
+
+// word-wrap: break-word;
+// word-break: break-all;
+// position: relative;
+function getArea(width) {
+  return height => {
+    console.log(width, height)
+    return width * height
+  }
+}
+
+const getTenWidthArea = getArea(10)
+// 之后碰到宽度为10的长方形就可以这样计算面积
+// const area1 = getTenWidthArea(20)
+getTenWidthArea(20)
+function fn(...args){
+  console.log(this,args);
+}
+var obj = {
+  myname:"张三"
+}
+
+fn.apply(obj,[1,2]); // this会变成传入的obj，传入的参数必须是一个数组；
+fn(1,2) // this指向window
--- begin/src/views/catalogue/HEALTH.vue
+++ begin/src/views/catalogue/HEALTH.vue
@@ -1,16 +1,77 @@
 
 <template>
 <div class="father">
-  <div></div>
+  <div>{{name}}</div>
   <el-tag
     v-for="tag in tags"
     :key="tag.name"
     :disable-transitions="false"
+    @click.native="Username(tag)"
     @close="handleClose(tag)"
     closable
     :type="tag.type">
     {{tag.name}}
   </el-tag>
+  <input
+    class="opopop"
+    type="text"
+    v-model="sendMessage"
+    @keyup.enter="handleSendButton()"
+    placeholder="请输入你要发送的消息">
+  <input
+    type="text"
+    v-model="username"
+    placeholder="请输入接收方id/name">
+  <button @click="handleLogoutButton()">退出</button>
+  <table class="ww">
+    <thead>
+    <tr>
+      <th>消息编号</th>
+      <th>发送者</th>
+      <th>接收者</th>
+      <th>发送时间</th>
+      <th>发送内容</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="item in messageList" >
+      <td>{{ item.id }}</td>
+      <td>{{ item.username }}</td>
+      <td>{{ username }}</td>
+      <td>{{ new Date(item.time).toLocaleTimeString() }}</td>
+      <td>{{ item.message }}</td>
+    </tr>
+    </tbody>
+  </table>
+  <div class="box">
+    <div class="left">
+
+    </div>
+    <div class="right">
+    <div class="title">在线咨询</div>
+    <div class="content">
+        <span v-for="(item,index) in messageList"  :class="{'right':index%2==0, 'left':index%2!=0,}">
+          <span>{{ item.id }}</span>
+          <span>{{ item.username }}</span>
+          <span>{{ username }}</span>
+          <span>{{ new Date(item.time).toLocaleTimeString() }}</span>
+          <span>{{ item.message }}</span>
+        </span>
+
+    </div>
+    <div class="bar">
+      <el-input
+        @keyup.native.enter="handleSendButton()"
+        class="input"
+        placeholder="请输入内容"
+        v-model="sendMessage"
+        clearable>
+      </el-input>
+      <el-button type="success"  class="send" @click="handleSendButton()">发送</el-button>
+
+    </div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -18,6 +79,11 @@
 export default {
   data(){
     return{
+      webSocketObject:null,
+      messageList:[],
+      name:'',
+      username:'',
+      sendMessage:'',
       tags: [
         { name: '标签一', type: '' },
         { name: '标签二', type: 'success' },
@@ -31,22 +97,133 @@ export default {
    init(){
 
    },
+    webSocketInit(){
+      const webSocketUrl = 'ws://192.168.0.76:8465/websocket/'+ this.name
+      this.webSocketObject = new WebSocket(webSocketUrl);
+      this.webSocketObject.onopen = this.webSocketOnOpen
+      this.webSocketObject.onmessage = this.webSocketOnMessage
+      // this.webSocketObject.onerror = this.webSocketOnError
+      this.webSocketObject.onclose = this.webSocketOnClose
+    },
+    webSocketOnMessage(e){
+      console.log('来自服务端的消息->',e)
+      const receiveMessage = JSON.parse(e.data);
+      this.messageList.push(receiveMessage)
+      var fatherElement = document.getElementsByClassName('father')[0];
+      fatherElement.scrollTop = fatherElement.scrollHeight - fatherElement.offsetHeight;
+
+    },
+    webSocketOnOpen(e){
+      console.log('与服务端连接打开->',e)
+    },
     handleClose(tag) {
       this.tags.splice(this.tags.indexOf(tag), 1);
     },
+    Username(tag){
+      // this.username=tag.name
+      // this.webSocketObject.close();
+      // this.webSocketInit()
+    },
+    handleSendButton() {
+      const username = this.name
+      const message = this.sendMessage
+      this.webSocketObject.send(JSON.stringify({
+        id: 1,
+        message,
+        username,
+        time: new Date().getTime()
+      }))
+      this.sendMessage = ''
+    },
+    handleLogoutButton(){
+     this.username=''
+      // removeUsername() //清除username然后断开连接
+      this.webSocketObject.close();
+
+    },
+    webSocketOnError(e){
+      console.log('与服务端连接异常->',e)
+    },
+    webSocketOnClose(e){
+      console.log('与服务端连接关闭->',e)
+    },
   },
   mounted(){
-
+    this.name=this.$store.state.A.user
+    this.webSocketInit()
   }
 }
 </script>
 
 <style scoped lang="scss">
 .father{
+  scroll-behavior: smooth;
+  overflow-y: auto;
   height: 100%;
   width: 100%;
   .el-tag{
     margin-right:20px ;
+  }
+}
+.opopop{
+  position: fixed;
+  top:20px;
+}
+.box{
+ border: #8c939d 2px solid;
+  border-radius: 10px;
+display: flex;
+  flex-direction: row;
+  width: 60%;
+  height: 70%;
+  position: absolute;
+  left: 50%;
+  top:50%;
+  transform: translate(-50%,-50%);
+  .left{
+   flex: 1;
+    background-color: #8c939d;
+  }
+  .right {
+    flex: 8;
+    .title {
+      display: flex;
+      align-items: center;
+      padding-left: 20px;
+      background-color: palevioletred;
+      font-family: "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+      height: 10%;
+    }
+
+    .content {
+      background-color: #f78989;
+      width: 100%;
+      height: 80%;
+     display: block;
+      flex-direction: column;
+      .left{
+        max-width: 80%;
+      }
+      .right{
+        max-width: 80%;
+
+      }
+    }
+
+    .bar {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 10%;
+      .input{
+        width: 80%;
+      }
+      .send{
+
+      }
+    }
   }
 }
 
